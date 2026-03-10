6930be0c11d3647922090904fc29ee1d0a5eab9b
kublakhan2 <github@khankhan.eu>
2026-03-10 18:58:04 +0100

    CSS animations; split js and css into separate files

---
+++ playback.js
@@ -0,0 +1,36 @@
+/*eslint semi: ["error", "always"]*/
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/DOMContentLoaded_event
+if (document.readyState === "loading") {
+  // Loading hasn't finished yet
+  document.addEventListener("DOMContentLoaded", setup_anchors);
+} else {
+  // `DOMContentLoaded` has already fired
+  setup_anchors();
+}
+
+function setup_anchors() {
+  console.info("DOM loaded");
+  document.querySelectorAll("a").forEach(anchor => {
+    anchor.addEventListener('click', play);
+    
+  });
+}
+
+function play(event) {
+  const anchor = event.currentTarget; // this == event.currentTarget?
+  const audio = anchor.firstChild;
+  console.info(`"${anchor.innerText}" wants to play ${audio.src}`);
+  audio.play().then(
+    () => { highlight(anchor); }, // fulfilled
+    () => { console.error('playback error'); } // rejected
+  );
+}
+
+function highlight(anchor) {
+  anchor.addEventListener("animationend", () => {
+    anchor.classList.remove('animated');
+    console.info(`animation ended on "${anchor.innerText}"`);
+  });
+  anchor.classList.add('animated');
+}
